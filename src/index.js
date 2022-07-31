17bcc9f84e0c07be3637b40fe91b7eb1f38a11ce
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-07-31 17:34:35 +0300

    Add items counter for the homepage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
-import addFooter from './modules/addFooter';
-import fetchData from './modules/fetchData';
+import addFooter from './modules/addFooter.js';
+import fetchData from './modules/fetchData.js';
+import countItems from './modules/itemsCounter.js';
 import addHeader from './modules/addHeader.js';
 
 addHeader();
 addFooter();
 fetchData();
+countItems();
